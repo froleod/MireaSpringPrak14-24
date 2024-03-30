ebd7ab97d9b593b197dd635957c304f09a169cd4
Leonid Frolov <froleod@gmail.com>
2024-03-30 18:29:09 +0300

    prak19 Logging

--- src/main/java/com/ldf/springprak14/Service/MarketService.java
+++ src/main/java/com/ldf/springprak14/Service/MarketService.java
@@ -6,41 +6,50 @@ import com.ldf.springprak14.Repo.MarketRepository;
 import com.ldf.springprak14.Repo.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MarketService {
     private final MarketRepository marketRepository;
     private final ProductRepository productRepository;
 
     public Market createMarket(Market market){
+        log.info("Market:" + market + "was created");
         return marketRepository.save(market);
     }
 
     public List<Market> findAll(){
+        log.info("Find all markets");
         return marketRepository.findAll();
     }
 
     public Market getMarketById(Long id){
+        log.info("Find market with id: " + id);
         return marketRepository.findById(id).orElse(null);
     }
 
     public List<Market> findMarketByName(String name){
+        log.info("Find market with name: " + name);
         return marketRepository.findMarketByName(name);
     }
 
     public List<Market> findMarketByAddress(String address){
+        log.info("Find market with address: " + address);
         return marketRepository.findMarketByAddress(address);
     }
 
     public void deleteMarket(Long id){
+        log.info("Market with id: " + id + " was deleted");
         marketRepository.deleteById(id);
     }
 
     public List<Product> getProductsByMarket(Long marketId) {
+        log.info("Find product by market with id: " + marketId);
         Market market = marketRepository.findById(marketId).orElse(null);
         if (market != null) {
             return market.getProducts();
@@ -50,6 +59,7 @@ public class MarketService {
 
     @Transactional
     public void addProductToMarket(Long marketId, Long productId) {
+        log.info("Add product with id: " + productId + " to market with id: " + marketId);
         Market market = marketRepository.findById(marketId).orElse(null);
         Product product = productRepository.findById(productId).orElse(null);
         if (market != null && product != null) {
--- src/main/java/com/ldf/springprak14/Service/ProductService.java
+++ src/main/java/com/ldf/springprak14/Service/ProductService.java
@@ -5,38 +5,46 @@ import com.ldf.springprak14.Entity.Product;
 import com.ldf.springprak14.Repo.MarketRepository;
 import com.ldf.springprak14.Repo.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
     private final MarketRepository marketRepository;
 
     public Product createProduct(Product product) {
+        log.info("Product was created");
         return productRepository.save(product);
     }
 
     public List<Product> getAllProducts() {
+        log.info("Find all products");
         return productRepository.findAll();
     }
 
     public Optional<Product> getProductById(Long id) {
+        log.info("Find product by id: " + id);
         return productRepository.findById(id);
     }
 
     public void deleteProduct(Long id) {
+        log.info("Delete product by id: " + id);
         productRepository.deleteById(id);
     }
 
     public Market getMarketByProduct(Long productId) {
+        log.info("Find market by product with id:" + productId);
         Optional<Product> productOptional = productRepository.findById(productId);
         return productOptional.map(Product::getMarket).orElse(null);
     }
 
     public void addProductToMarket(Long productId, Long marketId) {
+        log.info("Add product with id: " + productId + " to market with id: " + marketId);
         Optional<Product> productOptional = productRepository.findById(productId);
         Optional<Market> marketOptional = marketRepository.findById(marketId);
         if (productOptional.isPresent() && marketOptional.isPresent()) {
@@ -49,8 +57,8 @@ public class ProductService {
         }
     }
 
-    // Метод для фильтрации продуктов по имени и цене
     public List<Product> filterProducts(String name, Integer price) {
+        log.info("Filter product by name/price");
         if (name != null && price != null) {
             return productRepository.findByNameAndPrice(name, price);
         } else if (name != null) {
